b8e491a4c795659cfff69d46c03fe68b8856d007
Daniel Davidson <DanDavidson88@gmail.com>
2022-06-27 16:12:07 -0700

    updated profile pic
    Need a better photo. This is a placeholder

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0  justify-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Danny.
             <br className="hidden lg:inline-block" /> I love to build websites.
@@ -39,7 +39,7 @@ function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src={require('./../images/incognito.png')}
+            src={require('./../images/IMG_0150_2_60.jpg')}
           />
         </div>
       </div>
