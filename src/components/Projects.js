2b3861489de92c4aaa1ea8074465f051cf834e8c
Daniel Davidson <DanDavidson88@gmail.com>
2022-06-27 15:42:35 -0700

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,9 +11,7 @@ export default function Projects() {
             My Projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+            
           </p>
         </div>
 
