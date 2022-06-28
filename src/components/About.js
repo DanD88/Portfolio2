fe0dfa7697788261684a41fe30abbda8b31a7c94
Daniel Davidson <DanDavidson88@gmail.com>
2022-06-28 12:33:39 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0  justify-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Danny.
+            Hi, I'm Daniel.
             <br className="hidden lg:inline-block" /> I love to build websites.
           </h1>
           <p className="mb-8 leading-relaxed">
